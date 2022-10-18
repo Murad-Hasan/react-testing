1485ba3051466aac1475f6bff4d49b1cc59a3863
Murad-Hasan <muradhasan.7845@gmail.com>
2022-10-18 23:15:26 +0600

    group test

--- src/components/grid/Grid.test.tsx
+++ src/components/grid/Grid.test.tsx
@@ -1,24 +1,35 @@
 import { render, screen } from "@testing-library/react";
 import Grid from "./grid";
-// test("Great render correctly", () => {
-//   render(<Grid />);
-//   const textElement = screen.getByText(/hello/i);
-//   expect(textElement).toBeInTheDocument();
-// });
-
 
 // * Test driven development
-
-test('Grid renders correctly', () => {
-  render (<Grid />);
-  const textElement = screen.getByText('Hello');
+test.skip("renders correctly", () => {
+  render(<Grid />);
+  const textElement = screen.getByText("Hello");
   expect(textElement).toBeInTheDocument();
 });
 
-
-
-test('Grid renders with a name', () => {
-  render (<Grid name="John" />);
-  const textElement = screen.getByText('Hello John');
+test.skip("renders with a name", () => {
+  render(<Grid name="John" />);
+  const textElement = screen.getByText("Hello John");
   expect(textElement).toBeInTheDocument();
-})     
+});
+
+/*
+ * grouping tests using describe
+ */
+
+describe.only("Grid", () => {
+  test("renders correctly", () => {
+    render(<Grid />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+  // * nested describe
+  describe("nested", () => {
+    test("renders with a name", () => {
+      render(<Grid name="John" />);
+      const textElement = screen.getByText("Hello John");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
