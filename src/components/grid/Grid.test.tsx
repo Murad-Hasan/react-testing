5516835e98ef1b20d59f12b4b5673407046fbf9c
Murad-Hasan <muradhasan.7845@gmail.com>
2022-10-18 22:29:43 +0600

    TDD

--- src/components/grid/Grid.test.tsx
+++ src/components/grid/Grid.test.tsx
@@ -1,7 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Grid from "./grid";
-test("Great render correctly", () => {
-  render(<Grid />);
-  const textElement = screen.getByText(/hello/i);
+// test("Great render correctly", () => {
+//   render(<Grid />);
+//   const textElement = screen.getByText(/hello/i);
+//   expect(textElement).toBeInTheDocument();
+// });
+
+
+// * Test driven development
+
+test('Grid renders correctly', () => {
+  render (<Grid />);
+  const textElement = screen.getByText('Hello');
   expect(textElement).toBeInTheDocument();
 });
+
+
+
+test('Grid renders with a name', () => {
+  render (<Grid name="John" />);
+  const textElement = screen.getByText('Hello John');
+  expect(textElement).toBeInTheDocument();
+})
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,3 +1,11 @@
-export default function grid() {
-  return <div>Hello</div>;
+// export default function grid() {
+//   return <div>Hello</div>;
+// }
+
+type GridProps = {
+  name?: string;
+};
+
+export default function grid(props: GridProps) {
+  return <div> Hello {props.name}</div>;
 }
